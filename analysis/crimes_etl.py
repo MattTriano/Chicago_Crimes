3dc206a7e02a3d23a241a3db62b5da27e24fbe09
matttriano <matt.triano@gmail.com>
2022-03-26 20:18:08 -0500

    Submit sequential queries to pull all new or updated records since the last pull.

--- analysis/crimes_etl.py
+++ analysis/crimes_etl.py
@@ -88,3 +88,34 @@ def load_clean_chicago_crimes_data(
     else:
         crimes_gdf = gpd.read_parquet(clean_file_path)
     return crimes_gdf
+
+
+def get_chicago_crimes_data_since_latest_record(
+    crimes_gdf: gpd.GeoDataFrame,
+    table_id: str = "ijzp-q8t2",
+    filter_col: str = "updated_on",
+    socrata_domain: str = "data.cityofchicago.org",
+    count_col: str = "id",
+) -> pd.DataFrame:
+    api_call_base = f"https://{socrata_domain}/resource/{table_id}.csv"
+    latest_update = crimes_gdf[filter_col].max()
+    latest_update_str = latest_update.strftime(format="%Y-%m-%dT%H:%M:%S.000")
+    filter_str = f"$where={filter_col}>'{latest_update_str}'"
+
+    result_count = get_number_of_results_for_socrata_query(
+        filter_str=filter_str, api_call_base=api_call_base, count_col=count_col
+    )
+
+    df_parts = []
+    for i in range((result_count // 1000) + 1):
+        offset = i * 1000
+        if result_count < (offset + 1000):
+            limit = result_count % 1000
+        else:
+            limit = 1000
+        pagination_str = f"$limit={limit}&$offset={offset}&$order={count_col}"
+        api_call = f"{api_call_base}?{filter_str}&{pagination_str}"
+        df_parts.append(make_api_call_for_socrata_csv_data(api_call))
+    recent_crimes_df = pd.concat(df_parts)
+    recent_crimes_df = recent_crimes_df.reset_index(drop=True)
+    return recent_crimes_df
