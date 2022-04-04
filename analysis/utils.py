99f17ffeeceda6341e25e2d7a6f2dde991e1e671
matttriano <matt.triano@gmail.com>
2022-04-04 14:35:31 -0500

    Implements functionality to produce facilitate enhancing DataFrames to GeoDataFrames.

--- analysis/utils.py
+++ analysis/utils.py
@@ -60,6 +60,15 @@ def make_point_geometry(df: pd.DataFrame, long_col: str, lat_col: str) -> pd.Ser
     return df
 
 
+def geospatialize_df_with_point_geometries(
+    df: pd.DataFrame, long_col: str, lat_col: str, crs: str = "EPSG:4326"
+) -> gpd.GeoDataFrame:
+    df = df.copy()
+    gdf = make_point_geometry(df=df, long_col=long_col, lat_col=lat_col)
+    gdf = gpd.GeoDataFrame(gdf, crs=crs)
+    return
+
+
 def typeset_datetime_column(dt_series: pd.Series, dt_format: Optional[str]) -> pd.Series:
     dt_series = dt_series.copy()
     if not is_datetime64_any_dtype(dt_series):
